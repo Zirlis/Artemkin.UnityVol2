184fb4402bfab3d9efd3f8925a020bad154c1f0f
Zirlis <Zirlis@yandex.ru>
2021-02-08 22:24:13 +0700

    Add empty player

---
+++ Assets/Scripts/Foot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Foot : MonoBehaviour
+{
+    [SerializeField] Player Player;    
+    private float verticalSpeedMod;
+    private float nullVerticalSpeedMod;
+    private float nullForceOfDown;
+    private float maxForceOfDown;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+        {
+            Player.modSpeed = verticalSpeedMod;
+            Player.canJump = true;
+            Player.forceOfDown = nullForceOfDown;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            Player.modSpeed = nullVerticalSpeedMod;
+            Player.canJump = false;
+            Player.forceOfDown = maxForceOfDown;
+        }
+    }
+
+    void Start()
+    {        
+        verticalSpeedMod = Player.verticalSpeedMod;
+        nullVerticalSpeedMod = Player.nullVerticalSpeedMod;
+        nullForceOfDown = Player.nullForceOfDown;
+        maxForceOfDown = Player.maxForceOfDown;
+    }    
+}
